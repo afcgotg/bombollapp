a1d1a641e62156b2cbd23e03f9fadf170f6c4438
Alex Fernandez <fernialex@gmail.com>
2024-08-21 10:50:00 +0200

    Refactor on shop module

--- bombollapp/shop.py
+++ bombollapp/shop.py
@@ -46,46 +46,84 @@ def get_product(id):
 	).fetchone()
 	return product
 
+def get_product_from_form():
+	product = {}
+	product['reference'] = request.form['reference']
+	product['name'] = request.form['name']
+	product['description'] = request.form['description']
+	product['price'] = request.form['price']
+	product['in_bulk'] = "in_bulk" in request.form
+	product['stock'] = request.form['stock']
+	
+	error = validate_data(product)
+
+	return (product, error)
+	
+def validate_data(product):
+	error = None
+	if not product['reference']:
+		error = "Producte sense referència."
+	if not product['name']:
+		error = "Producte sense nom."
+	if product['price']:
+		try:
+			product['price'] = float(product['price'])
+		except ValueError:
+			error = "El preu ha de ser un valor numèric."
+	if product['stock']:
+		try:
+			product['stock'] = int(product['stock'])
+		except ValueError:
+			error = "L'stock ha de ser un nombre enter."
+		else:
+			if product['stock'] < 0:
+				error = "L'stock ha de ser un nombre positiu."	
+	return error
+
+def query_format(product, id=None):
+	product_tuple = (product['reference'], product['name'],
+		product['description'], product['price'], product['in_bulk'],
+		product['stock'])
+	if id:
+		product_tuple = product_tuple + (id,)
+	return product_tuple
 
 @bp.route('/create', methods=('GET', 'POST'))
-@bp.route('/update/<int:id>', methods=('GET', 'POST'))
 @admin_login_required
-def upsert(id=None):
+def create():
 	product=None
-	if id:
-		product = get_product(id)
-
 	if request.method == 'POST':
-		reference = request.form['reference']
-		name = request.form['name']
-		description = request.form['description']
-		price = request.form['price']
-		in_bulk = "in_bulk" in request.form
-		stock = request.form['stock']
-		
-		error = None
-		if not reference:
-			error = "Producte sense referència."
-		if not name:
-			error = "Producte sense nom."
-
+		product, error = get_product_from_form()
 		if error is not None:
 			flash(error)
 		else:
 			db = get_db()
-			if id:
-				db.execute(
-					'UPDATE product SET reference = ?, name = ?, '
-					'description = ?, price = ?, in_bulk = ?, stock = ? '
-					'WHERE id = ?',
-					(reference, name, description, price, in_bulk, stock, id)
-				)
-			else:
-				db.execute(
-					'INSERT INTO product (reference, name, description, price, '
-					'in_bulk, stock) VALUES (?, ?, ?, ?, ?, ?)',
-					(reference, name, description, price, in_bulk, stock)
-				)
+			db.execute(
+				'INSERT INTO product (reference, name, description, price, '
+				'in_bulk, stock) VALUES (?, ?, ?, ?, ?, ?)',
+				query_format(product)
+			)
+			db.commit()
+			return redirect(url_for('shop.panel'))
+	
+	return render_template('shop/form.html', product=product)
+
+@bp.route('/update/<int:id>', methods=('GET', 'POST'))
+@admin_login_required
+def update(id=None):
+	product = get_product(id)
+	if request.method == 'POST':
+		product, error = get_product_from_form()
+		if error is not None:
+			flash(error)
+		else:
+			db = get_db()
+			db.execute(
+				'UPDATE product SET reference = ?, name = ?, '
+				'description = ?, price = ?, in_bulk = ?, stock = ? '
+				'WHERE id = ?',
+				query_format(product, id)
+			)
 			db.commit()
 			return redirect(url_for('shop.panel'))
 	
